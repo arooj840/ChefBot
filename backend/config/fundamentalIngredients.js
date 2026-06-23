dc47b4696e8ca4fa96ef7d8322fdcb3062253c57
aroojfatima0630 <fatimaproject122@gmail.com>
2026-06-23 11:40:25 -0700

    backend code

--- backend/config/fundamentalIngredients.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  common: ['salt', 'oil', 'red chilli'],
-  veg: ['tomato'],
-  nonVeg: ['meat'],
-  meatKeywords: ['chicken', 'mutton', 'beef', 'fish', 'meat', 'lamb', 'goat']
-};
